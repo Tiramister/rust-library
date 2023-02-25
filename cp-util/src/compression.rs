41e46d99656ff7b7a1064d1ed0e918f92409d0a4
Tiramister <nh111424@gmail.com>
2023-02-25 14:53:57 +0000

    座標圧縮を追加

--- cp-util/src/compression.rs
+++ cp-util/src/compression.rs
@@ -12,3 +12,57 @@ where
     }
     ret
 }
+
+pub struct Compressor<T>
+where
+    T: Ord + Clone,
+{
+    values: Vec<T>,
+}
+
+impl<T> Compressor<T>
+where
+    T: Ord + Clone,
+{
+    pub fn new(values: &[T]) -> Self {
+        let mut values = values.to_owned();
+        values.sort();
+        values.dedup();
+        Self {
+            values: values.to_owned(),
+        }
+    }
+
+    pub fn values(&self) -> Vec<T> {
+        self.values.clone()
+    }
+
+    pub fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    pub fn value(&self, index: usize) -> T {
+        self.values[index].clone()
+    }
+
+    pub fn index(&self, value: T) -> Option<usize> {
+        // values[l] <= value
+        let mut l = 0;
+        let mut r = self.values.len();
+
+        while r - l > 1 {
+            let mid = (l + r) / 2;
+            if self.values[mid] <= value {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+
+        if self.values[l] == value {
+            Some(l)
+        } else {
+            None
+        }
+    }
+}
